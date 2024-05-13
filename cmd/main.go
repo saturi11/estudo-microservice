aa72b8fcae93723c18c16b61910862ee678d6d42
Gabriel Saturi <gabrielsaturi11@gmail.com>
2024-05-13 15:54:59 -0300

    chore: Configure Kafka consumer and process incoming messages

--- cmd/main.go
+++ cmd/main.go
@@ -13,20 +13,27 @@ import (
 )
 
 func main() {
+	// Open a connection to the SQLite database
 	db, error := sql.Open("sqlite3", "test.db")
 	if error != nil {
 		log.Fatal(error)
 	}
+
+	// Create a repository factory using the database connection
 	repositoryFactory := factory.NewRepositoryDatabaseFactory(db)
 	repository := repositoryFactory.CreateTransactionRepository()
 
+	// Configure the Kafka producer
 	configMapProducer := &ckafka.ConfigMap{
 		"bootstrap.servers": "kafka:9092",
 	}
 	kafkaPresenter := transaction.NewTransactionalKafkaPresenter()
 	producer := kafka.NewKafkaProducer(configMapProducer, kafkaPresenter)
 
+	// Create a channel to receive Kafka messages
 	var msgChan = make(chan *ckafka.Message)
+
+	// Configure the Kafka consumer
 	configMapConsumer := &ckafka.ConfigMap{
 		"bootstrap.servers": "kafka:9092",
 		"client.id":         "goapp",
@@ -34,10 +41,14 @@ func main() {
 	}
 	topics := []string{"transactions"}
 	consumer := kafka.NewKafkaConsumer(configMapConsumer, topics)
+
+	// Start consuming messages from Kafka in a separate goroutine
 	go consumer.Consume(msgChan)
 
+	// Create the use case for processing transactions
 	usecase := process_transaction.NewProcessTransaction(repository, producer, "transactions_result")
 
+	// Process incoming messages from Kafka
 	for msg := range msgChan {
 		var input process_transaction.TransactionalDTOInput
 		json.Unmarshal(msg.Value, &input)
